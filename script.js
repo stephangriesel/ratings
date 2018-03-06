7ea7988e7ea81ffbabf18aba503a4a6f24cf2f15
Stephan Griesel <sgriesel@gmail.com>
2018-03-06 19:00:39 +0100

    update - not working

--- script.js
+++ script.js
@@ -14,6 +14,38 @@ const starsTotal = 5;
 // Run get ratins when dom loads
 document.addEventListener('DOMContentLoaded', getRatings);
 
+// Form element 
+const productSelect = document.getElementById('product-select');
+const ratingControl = document.getElementById('rating-control');
+
+// Initialize product
+let product;
+
+// Product select event
+productSelect.addEventListener('change', (e) => {
+    product = e.target.value;
+    // Enable rating control
+    ratingControl.disabled = false;
+    ratingControl.value = ratings[product];
+});
+
+// Rating control change
+
+ratingControl.addEventListener('blur', (e) => {
+    const rating = e.target.value;
+
+    //check for undervalue
+    if(rating > 5) {
+        alert('Please rate 1 - 5');
+        return;
+    }
+
+    // Change ratinh
+    ratings[product] = rating;
+
+    getRatings();
+});
+
 // For in loop. Get ratings
 function getRatings() {
     for(let rating in ratings) {
@@ -22,9 +54,15 @@ function getRatings() {
         
         // Round to nearest 10
         const starPercentageRounded = `${Math.round(starPercentage / 10) * 10}%`;
-        console.log(starPercentageRounded);
+        // console.log(starPercentageRounded);
 
         // Set width of stars inner to %
-        document.querySelector(`.${rating} .stars-inner`).style.width = starPercentageRounded;
+        document.querySelector(`.${rating} .stars-inner`)
+        .style.width = starPercentageRounded;
+
+        // Add number rating
+        document.querySelector(`.${rating} .number-rating`)
+        .innerHTML = ratings[rating];
+
     }
 }
